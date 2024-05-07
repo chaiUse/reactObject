fbe3f8837bd3fffea3f11a640174eb932d052f0f
孙瑞敏 <272293971@qq.com>
2024-05-07 20:34:05 +0800

    班级

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.sscss";
+import "./index.scss";
 import { HashRouter } from "react-router-dom";
 import store from "./store/store.jsx";
 import { Provider } from "react-redux";
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,6 +2,8 @@ import Home from "../pages/Home/Home";
 import RoleManagement from "../pages/Home/chai/RoleManagement/roleManagement";
 import Login from "../pages/Login/Login";
 import Room from "../pages/Home/Room/Room";
+import Students from "../pages/Home/Room/Students";
+
 import { lazy } from "react";
 const TestDetail = lazy(() => import('../pages/Home/studentSystem/testDetail'))
 const TestList = lazy(() => import('../pages/Home/studentSystem/testList'))
@@ -15,12 +17,8 @@ const routeConfige=[
     path:'/home',
     element: <Home />,
     children:[
-      {
-        path:'/home/room',
-        element:<Room />
-      }
-    ],
-    children:[
+      {path:'/home/room',element:<Room />},
+      {path:'/home/students',element:<Students />},
       { path:'testDetail', element:<TestDetail /> },
       { path:'testList', element:<TestList /> },
       { path:'overTest', element:<OverTest /> },
