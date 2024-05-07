56b8a2c42a13e7e29035f04b4a267c8942da2f6c
王帅 <1824991182@qq.com>
2024-05-07 21:19:04 +0800

    flx

--- src/tool/Rout.js
+++ src/tool/Rout.js
@@ -1,3 +1,29 @@
-export const router = (ls, lt) => {
-  console.log(ls, lt);
+import { useNavigate } from "react-router-dom";
+
+import {
+  InboxOutlined,
+  TeamOutlined,
+  ProjectOutlined,
+  FileTextOutlined,
+  SignatureOutlined,
+} from "@ant-design/icons";
+
+const navigate = useNavigate();
+export const router = (lt) => {
+  const res = lt.map((item) => {
+    return {
+      key: item.path,
+      icon: <InboxOutlined />,
+      label: item.name,
+      children: item.children.map((i) => {
+        return {
+          key: i.path,
+          label: i.name,
+          onTitleClick: () => navigate(i.path),
+        };
+      }),
+    };
+  });
+  console.log(res);
+  return res;
 };
