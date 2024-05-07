78b9c774a05332dba6fac74b46692ba49281430f
王帅 <1824991182@qq.com>
2024-05-07 11:27:54 +0800

    登录功能

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 // import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { HashRouter } from 'react-router-dom'
-import store from './store/store.jsx'
-import { Provider } from 'react-redux'
-
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { HashRouter } from "react-router-dom";
+import store from "./store/store.jsx";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
