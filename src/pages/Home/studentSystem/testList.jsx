bbfddc3669c60716c1cf078bbdca156090449da1
任校庆 <2016889468@qq.com>
2024-05-07 11:29:19 +0800

    hb

---
+++ src/pages/Home/studentSystem/testList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const TestList = () => {
+    return (
+        <div>考试列表</div>
+    )
+}
+
+export default TestList
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,6 +1,9 @@
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
-
+import { lazy } from "react";
+const TestDetail = lazy(() => import('../pages/Home/studentSystem/testDetail'))
+const TestList = lazy(() => import('../pages/Home/studentSystem/testList'))
+const OverTest = lazy(() => import('../pages/Home/studentSystem/overTest'))
 const routeConfige=[
   {
     path:'/',
@@ -8,7 +11,12 @@ const routeConfige=[
   },
   {
     path:'/home',
-    element: <Home />
+    element: <Home />,
+    children:[
+      { path:'testDetail', element:<TestDetail /> },
+      { path:'testList', element:<TestList /> },
+      { path:'overTest', element:<OverTest /> }
+    ]
   }
 ]
 export default routeConfige
