3f78a33ded48b797f1bf3ba5a9e9462f7bbb86a4
柴虎虎 <2337895722@qq.com>
2024-05-07 10:17:14 +0800

    d

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import eslintPlugin from 'vite-plugin-eslint'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      include:['src/**/*.js','src/**/*.jsx']
+    })
+  ],
 })
