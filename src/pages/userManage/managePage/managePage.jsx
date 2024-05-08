c63ab3d34c6b1fedde23c177b065cc9d9e71af14
王帅 <1824991182@qq.com>
2024-05-08 08:57:05 +0800

    flx

---
+++ src/pages/userManage/managePage/managePage.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import style from './manage.module.scss'
+
+function managePage() {
+  return (
+    <div className={style.manage}>
+      <h2>用户管理</h2>
+    </div>
+  );
+}
+
+export default managePage;
