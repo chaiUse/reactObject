08d3cc7c543f0b964b41d1f52f2a10dd5ee58fb6
任校庆 <2016889468@qq.com>
2024-05-13 22:29:25 +0800

    hb

--- src/api/permissionMenu/permissionMenu.jsx
+++ src/api/permissionMenu/permissionMenu.jsx
@@ -1,12 +1,13 @@
 import instance from "../api";
 //创建菜单
-export const createPre = (_id,name,path,disabled) => {
-    console.log('_id,name,path,disabled',_id,name,path,disabled);
+export const createPre = (_id,name,path,disabled,page) => {
+    console.log('_id,name,path,disabled,page',_id,name,path,disabled,page);
     return instance.post('/permission/create',{
         name,
         pid:_id,
         path,
-        disabled
+        disabled:JSON.parse(disabled),
+        page
     })
 }
 
@@ -16,16 +17,20 @@ export const findPre = () => {
 }
 
 //编辑菜单
-export const updatePre = (_id,name) => {
-    // console.log(_id,name,99999);
+export const updatePre = (_id,name,path,page,disabled) => {
+    console.log(_id,name,path,page,disabled);
     return instance.post('/permission/update',{
-        id:name,
-        name:_id,
+        id:_id,
+        name,
+        path,
+        page,
+        disabled
     })
 }
 
 //删除菜单
 export const deletePre = (_id) => {
+    console.log(6666,_id);
     return instance.post('/permission/remove',{
         id:_id
     })
